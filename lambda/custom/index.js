7744eaa8e2cf90c502fa42496c48cc2d6d77afb7
AziFarjad <azi.farjad7@gmail.com>
2018-09-18 18:30:56 +1000

    Update index.js

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -1,3 +1,9 @@
+let message = "<say-as interpret-as='interjection'>g'day</say-as>";
+message += "<break time='1s'/>" + " What is your name?”;
+
+let message = "<say-as interpret-as='interjection'>kia ora</say-as>";
+message += "<break time='1s'/>" + " What is your name?";
+
 const Alexa = require('ask-sdk');
 
 const SampleHandler = {
